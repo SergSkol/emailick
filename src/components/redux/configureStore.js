dadbe60a533495429e8a0a5be540965dc6214a9f
Sergiy Shkolnik <sps676@gmail.com>
2024-05-15 08:41:17 +0200

    use proxy

--- src/components/redux/configureStore.js
+++ src/components/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+// import logger from 'redux-logger';
 import messageReducer from './messages';
 
 const store = configureStore({
   reducer: {
     message: messageReducer,
+    // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   },
 });
 
